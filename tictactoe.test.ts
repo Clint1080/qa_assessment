f49af7df9222056521785a7ab684c49ac4d60b0c
Clint Edwards <cedwards1080@gmail.com>
2021-08-27 10:50:58 -0600

    Got two tests working that's right I'm amazing

--- tictactoe.test.ts
+++ tictactoe.test.ts
@@ -12,9 +12,34 @@ afterAll(async () => {
     await driver.quit()
 })
 
-test('I can start a game', async () => {
+describe('Tic Tac Toe tests for days', () => {
 
-    let button = await (await driver).findElement(By.id('start-game'));
-    await button.click();
+    test('I can start a game', async () => {
+        
+        let button = await (await driver).findElement(By.id('start-game'));
+        await button.click();
+        await driver.sleep(1000)
+        
+    });
+
+    test('Clicking the upper left square adds an X to the square', async () => {
+
+        let clickSquare0 = await (await driver).findElement(By.id('cell-0'));
+        await clickSquare0.click();
+        let clickSquare0Value = await (await driver).findElement(By.id('cell-0')).getAttribute('innerText')
+        expect(clickSquare0Value).toBe('X')
+        await driver.sleep(1000);
+
+    })
+
+     test('Clicking the upper right square adds an X to the square', async () => {
+
+        let clickSquare2 = await (await driver).findElement(By.id('cell-2'));
+        await clickSquare2.click();
+        let clickSquare2Value = await (await driver).findElement(By.id('cell-2')).getAttribute('innerText');
+        expect(clickSquare2Value).toBe('X')
+        await driver.sleep(1000);
+
+    })
     
-});
+})
