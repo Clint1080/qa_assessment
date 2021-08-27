4f451efb5c3d59c098e3771bd77465b3f1b9e5f6
Clint Edwards <cedwards1080@gmail.com>
2021-08-27 10:59:07 -0600

    added my third test and added some documentation of what our tests are doing

--- tictactoe.test.ts
+++ tictactoe.test.ts
@@ -12,8 +12,10 @@ afterAll(async () => {
     await driver.quit()
 })
 
-describe('Tic Tac Toe tests for days', () => {
+// Added all of the driver.sleep(1000) to intentially slow it down so I could watch the computer bow to my will!!!!!!
 
+describe('Tic Tac Toe tests for days', () => {
+    // All tests in this block depend on the start button being clicked
     test('I can start a game', async () => {
         
         let button = await (await driver).findElement(By.id('start-game'));
@@ -23,16 +25,24 @@ describe('Tic Tac Toe tests for days', () => {
     });
 
     test('Clicking the upper left square adds an X to the square', async () => {
-
+        // Selecting the square we want to fill 
         let clickSquare0 = await (await driver).findElement(By.id('cell-0'));
+
+        //clicking that square
         await clickSquare0.click();
+
+        // Now that we have clicked we need to get that squares value
         let clickSquare0Value = await (await driver).findElement(By.id('cell-0')).getAttribute('innerText')
+
+        // What do we expect the value of the cell to be 
         expect(clickSquare0Value).toBe('X')
+
+        // I wanted it to go a little slower so I can watch and enjoy 
         await driver.sleep(1000);
 
     })
 
-     test('Clicking the upper right square adds an X to the square', async () => {
+    test('Clicking the upper right square adds an X to the square', async () => {
 
         let clickSquare2 = await (await driver).findElement(By.id('cell-2'));
         await clickSquare2.click();
@@ -42,4 +52,13 @@ describe('Tic Tac Toe tests for days', () => {
 
     })
     
+    test('Clicking the lower right square adds an X to the square', async () => {
+
+        let clickSquare8 = await (await driver).findElement(By.id('cell-8'));
+        await clickSquare8.click();
+        let clickSquare8Value = await (await driver).findElement(By.id('cell-8')).getAttribute('innerText');
+        expect(clickSquare8Value).toBe('X')
+        await driver.sleep(1000);
+
+    })
 })
